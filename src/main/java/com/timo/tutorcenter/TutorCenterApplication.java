c66177d9a7550c1dcc60e3a71aec7ca8faa258c0
Timo <developer.timo@gmail.com>
2022-06-07 06:29:12 +0900

    Initial commit

---
+++ src/main/java/com/timo/tutorcenter/TutorCenterApplication.java
@@ -0,0 +1,13 @@
+package com.timo.tutorcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timo/tutorcenter/TutorCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timo.tutorcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
